862871a4314d060ce7cfb0710bec085900ffbfbb
Michael Nnaji <mnnaji3@uwo.ca>
2020-03-31 03:37:36 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,28 @@
+class Person
+
+
+def initialize(name,job)
+  @name = name
+  @job = job
+end
+
+def name
+  @name
+end
+
+def name=(new_name)
+  @name = newname
+end
+
+def job
+  @job
+end
+
+def job=(new_job)
+  @job = newjob
+end
+
+    
+
+
+end
